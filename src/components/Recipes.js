a26b6e5940841927053f5d9c6035a16824c80a20
nevraka <nevrakaya@gmail.com>
2020-12-04 21:50:00 +0100

    mounting useContext

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,36 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { Image, Card } from 'semantic-ui-react';
+import React, { useState, useEffect, useContext } from 'react';
+import RecipeCard from './RecipeCard';
+import { Header } from 'semantic-ui-react';
+import { DataContext } from '../context/DataContext';
+import useDebounce from '../hooks/useDebounce';
+import './header.css';
 
-const APP_ID = '3f0c57f3';
-const APP_KEY = 'c4e827c99045c3d42323a5bfc687041c';
+const APP_ID = process.env.REACT_APP_APP_ID;
+const APP_KEY = process.env.REACT_APP_APP_KEY;
 
 function Recipes() {
+  const [searchTerm] = useContext(DataContext);
   const [recipes, setRecipes] = useState([]);
+  const debouncedSearchTerm = useDebounce(searchTerm, 750);
 
   useEffect(() => {
     async function fetchMyAPI() {
       let response = await fetch(
-        `https://api.edamam.com/search?q=food&app_id=${APP_ID}&app_key=${APP_KEY}`
+        `https://api.edamam.com/search?q=${debouncedSearchTerm}&app_id=${APP_ID}&app_key=${APP_KEY}`
       );
       response = await response.json();
       setRecipes(response.hits);
       console.log(response.hits);
     }
     fetchMyAPI();
-  }, []);
+  }, [debouncedSearchTerm]);
 
   return (
-    <div>
-      <h1>count: {recipes.length}</h1>
-      {recipes.map((recipe) => {
-        return (
-          <Card
-            image={recipe.recipe.image}
-            header={recipe.recipe.label}
-            meta={recipe.recipe.healthLabels}
-          />
-        );
-      })}
+    <div className="page-header">
+      <Header as="h1" icon="food" content="Be Master in Your Kitchen" />
+      <RecipeCard recipes={recipes}></RecipeCard>
     </div>
   );
 }
